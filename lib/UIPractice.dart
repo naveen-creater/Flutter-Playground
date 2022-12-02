1d61e969ab4115d4c89e448e3109800665448e01
Naveenkumar.k <naveenkumar.k@marketsimplified.com>
2022-12-02 07:18:25 +0530

    Lottie Animation practice

--- lib/UIPractice.dart
+++ lib/UIPractice.dart
@@ -86,6 +86,68 @@ Widget padding() {
   );
 }
 
+Widget sizeBox() {
+
+  return SizedBox(
+    height: 100,
+    width: 200,
+    child: Card(
+      color: Colors.brown,
+      child: Center(
+        child: Text("center Text!", style: TextStyle(backgroundColor: Colors.deepOrange),),
+      ),
+    ),
+  );
+}
+
+Widget expanded(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Expanded(
+        child: container(100, 200, Colors.cyanAccent),
+        flex: 1,
+      ),
+      Expanded(
+        child: container(100, 200, Colors.black87),
+        flex: 3,
+      ),
+      Expanded(
+        child: container(100, 200, Colors.amberAccent),
+        flex: 4,
+      ),
+    ],
+  );
+}
+
+Widget flexible(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    mainAxisSize: MainAxisSize.max,
+    // crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Flexible(
+        child: Container(color: Colors.cyanAccent, height: 80, width: 80),
+        // fit: FlexFit.tight,
+        flex: 2,
+      ),
+      Flexible(
+        child: Container(color: Colors.indigoAccent, height: 80, width: 80),
+        flex: 6,
+        // fit: FlexFit.tight,
+      ),
+      Flexible(
+        child: Container(color: Colors.orange, height: 80, width: 80),
+        flex: 8,
+        // fit: FlexFit.tight,
+      ),
+    ],
+  );
+}
+
+
+
 class UIPracticeStae extends State<UIPractice> {
   @override
   Widget build(BuildContext context) {
@@ -94,7 +156,7 @@ class UIPracticeStae extends State<UIPractice> {
         appBar: AppBar(
           title: Text("UI Practice!"),
         ),
-        body: padding(),
+        body: flexible(),
       ),
     );
   }
