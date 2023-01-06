3a3dd89d0abf1172dafd52b952ca33c73977ec35
Naveenkumar.k <naveenkumar.k@marketsimplified.com>
2023-01-06 13:47:11 +0530

    aboutWidget

---
+++ lib/Widgets/AboutDialog.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(AboutDialogWidget());
+
+class AboutDialogWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return AboutDialogWidgetState();
+  }
+}
+
+class AboutDialogWidgetState extends State<AboutDialogWidget>{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+
+}
